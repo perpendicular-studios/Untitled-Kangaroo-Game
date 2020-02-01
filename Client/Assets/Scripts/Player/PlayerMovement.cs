493f292d8ee7853c0fd7a79e75fc09a0872226b8
kozr <zxzflow@gmail.com>
2020-02-01 00:24:13 -0800

    Initial commit

---
+++ Client/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 5000f;
+    public float sidewayForce = 5000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(- sidewayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, - forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
