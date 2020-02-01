d10763287cd08253ae8d95b8b453396f87c483fd
kozr <zxzflow@gmail.com>
2020-02-01 03:13:36 -0800

    added enemy basics

---
+++ Assets/Scripts/Controller/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    // Start is called before the first frame update
+
+    Transform target;
+    NavMeshAgent agent;
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponentInParent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/Scripts/GameManager/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    #region Singleton
+
+    public static PlayerManager instance;
+
+    void Awake() {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+}
