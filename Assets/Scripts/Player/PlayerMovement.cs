d0d15945b0917bb85d972a1c18179d05832b2ae9
wind711 <wind711@students.cs.ubc.ca>
2020-02-01 14:23:06 -0800

    Fixed model and jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,33 +5,21 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
     public PlayerStats stats;
-
-    
+    public float groundDist;
 
     public LayerMask groundLayers;
 
-    public CapsuleCollider col;
+    public BoxCollider col;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        col = GetComponent<CapsuleCollider>();
+        col = GetComponent<BoxCollider>();
         stats = GetComponentInChildren<PlayerStats>();
+        groundDist = col.bounds.extents.y;
     }
 
-    void FixedUpdate()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
-           GetComponent<Rigidbody>().velocity = Vector3.up * stats.jumpVelocity;
-        }
-        if (rb.velocity.y < 0) {
-            rb.velocity += Vector3.up * Physics.gravity.y * (stats.fallMultiplier - 1) * Time.deltaTime;
-        }
-    }
-
-
-    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -39,12 +27,17 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 playerMovement = new Vector3(horizontal, 0f, vertical) * stats.entitySpeed * Time.deltaTime;
         transform.Translate(playerMovement, Space.Self);
-        // transform.position = transform.position + new Vector3(horizontal * stats.entitySpeed * Time.deltaTime, 0, vertical * stats.entitySpeed * Time.deltaTime);
+        
+         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+           rb.velocity = Vector3.up * stats.jumpVelocity;
+        }
+        if (rb.velocity.y < 0) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (stats.fallMultiplier - 1) * Time.deltaTime;
+        }
     }
 
     private bool IsGrounded()
     {
-       return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, 
-       col.bounds.center.z), col.radius * .8f, groundLayers);
+       return Physics.Raycast(transform.position, -Vector3.up, groundDist + 0.2f);
     }
 }
