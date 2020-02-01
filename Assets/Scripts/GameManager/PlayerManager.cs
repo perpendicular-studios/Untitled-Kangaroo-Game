04f73c47272be6ca7c2f6cf211b5ab1028fb7a6e
Archiving <solarcactus@live.com>
2020-02-01 07:11:35 -0800

    Added gm as prefab

--- Assets/Scripts/GameManager/PlayerManager.cs
+++ Assets/Scripts/GameManager/PlayerManager.cs
@@ -4,37 +4,18 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public Interactable focus;
-    // Start is called before the first frame update
-    #region Singleton
-
     public static PlayerManager instance;
+    public PlayerMovement player;
 
     void Awake() {
         instance = this;
     }
 
-    #endregion
 
 
     void Update()
     {
-        if (Input.GetKeyDown("e"))
-        {
-            Interactable interactable = GetComponent<Interactable>();
-            if (interactable != null) {
-                Focus = interactable;
-            } else {
-                Focus = null;
-            }
-        }
+
     }
 
-    public Interactable Focus
-    {
-        get { return focus; }
-        set { focus = value; }
-    }
-
-    public GameObject player;
 }
