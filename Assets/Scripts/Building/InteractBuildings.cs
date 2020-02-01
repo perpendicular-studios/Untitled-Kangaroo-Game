7bd443eefec0916f00f90f37ebc6e504507dbff3
Matthew Raynard <matthewraynard65@gmail.com>
2020-02-01 12:38:07 -0800

    reeee

---
+++ Assets/Scripts/Building/InteractBuildings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractBuildings : BrokenFireStation
+{
+    private bool bIsUsed = false;
+
+    public override void onInteract()
+    {
+        InteractText = "Press E to Interact";
+        bIsUsed = !bIsUsed;
+    }
+}
