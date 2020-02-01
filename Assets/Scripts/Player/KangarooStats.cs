45bf296f14b2267f0ca251814c160bd03f0ff423
Matthew Raynard <matthewraynard65@gmail.com>
2020-02-01 01:29:13 -0800

    kangaroo stats

---
+++ Assets/Scripts/Player/KangarooStats.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class KangarooStats : MonoBehaviour
+{
+
+    public float Health;
+    public float HealthOverTime;
+
+    public Slider HealthBar;
+
+    Rigidbody myBody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+        HealthBar.maxValue = Health;
+
+
+        updateUI();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ValueChanges();
+
+    }
+
+    private void ValueChanges() {
+
+        if (Health < 0) {
+            Debug.Log("nibba ded");
+        }
+    
+    
+    }
+
+    private void updateUI() {
+
+        Health = Mathf.Clamp(Health, 0, 100);
+
+        HealthBar.value = Health;
+    
+    }
+
+    public void TakeDamage(float num) {
+
+        Health -= num;
+        if (Health < 0) {
+            Health = 0;
+        }
+        updateUI();
+
+    }
+
+
+}
