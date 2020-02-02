6436d1a545983e88cb8e69a9fb03f6e602acc432
wind711 <wind711@students.cs.ubc.ca>
2020-02-02 07:35:27 -0800

    added endsceen and gamemanager

---
+++ Assets/Scripts/GameManager/GameOver.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public void EndGame ()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Loser();
+        }
+    }
+
+    void Loser ()
+    {
+        SceneManager.LoadScene("EndScreen");
+    }
+
+}
