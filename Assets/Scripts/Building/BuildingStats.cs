8bf2edc067e7c66bd7eaae55f3e1a2debf3f4df7
Matthew Raynard <matthewraynard65@gmail.com>
2020-02-01 22:54:06 -0800

    Building Stats, pls help with regen area no clu

---
+++ Assets/Scripts/Building/BuildingStats.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingStats : MonoBehaviour
+{
+    [Header("Building Stats")]
+    public int maxHealth;
+    public int healthThreshold;
+    Material[] materials;
+
+    Renderer rend;
+
+    public string InteractText = "Press E to Interact";
+
+    public int currentHealth
+    {
+        get;
+        private set;
+    }
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = materials[0];
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log(currentHealth);
+        if(currentHealth <= 0)
+        {
+            Destroyed();
+        }
+    }
+
+    public virtual void Oninteract()
+    {
+    }
+
+
+    public virtual void Destroyed()
+    {
+        Debug.Log(transform.name + "poof ...");
+        rend.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Building/CactusStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusStats : BuildingStats
+{
+    public EntityStats stats;
+
+    private void Awake()
+    {
+        maxHealth = 100;
+    }
+
+
+    public override void Oninteract()
+    {
+        InteractText = "Press E to Interact";
+        stats.playerWater += 15;
+
+    }
+
+
+
+}
